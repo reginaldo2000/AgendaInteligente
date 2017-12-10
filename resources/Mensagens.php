70c3db9bed154549d1efd2767b6453353d661822
Reginaldo <Reginaldo@Reginaldo-PC>
2017-12-10 10:43:00 -0300

    requisito categoria

--- resources/Mensagens.php
+++ resources/Mensagens.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Mensagens
- *
- * @author Reginaldo
- */
 class Mensagens {
 
     function __construct() {
@@ -19,6 +8,10 @@ class Mensagens {
 
     public static function getMsgSuccess($stringMsg) {
         return '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'.$stringMsg.'</div>';
+    }
+    
+    public static function getMsgError($stringMsg) {
+        return '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'.$stringMsg.'</div>';
     }
 
 }
